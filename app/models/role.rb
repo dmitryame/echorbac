4709137701a0317d312042ec1b2a9a92eed864ad
dmitryame <dmitry@custommode.com>
2009-12-25 00:30:55 -0500

    adding subjects and resources to role model

--- app/models/role.rb
+++ app/models/role.rb
@@ -4,4 +4,12 @@ class Role
 
   key :name, String, :index => true
 
+  def subjects
+    res = Subject.find_by_role(self) #TODO: eventually replace with appropriate assitiation once mondomapper starts supporting it
+    res = Array.new if res == nil
+  end
+  def resources
+    res = Resource.find_by_role(self) #TODO: eventually replace with appropriate assitiation once mondomapper starts supporting it
+    res = Array.new if res == nil
+  end
 end
